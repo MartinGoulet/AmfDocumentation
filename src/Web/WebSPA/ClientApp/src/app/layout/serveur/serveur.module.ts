6a3e931ae5f797f0c4954cec80698c6c8dbbe036
Martin Goulet <martin.goulet@lautorite.qc.ca>
2018-10-22 15:28:11 -0400

    Ajout traduction espagnol + enlever élément de menu à gauche

--- src/Web/WebSPA/ClientApp/src/app/layout/serveur/serveur.module.ts
+++ src/Web/WebSPA/ClientApp/src/app/layout/serveur/serveur.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { CommonModule } from '@angular/common';
 
 import { ServeurRoutingModule } from './serveur-routing.module';
@@ -6,7 +7,7 @@ import { ServeurComponent } from './serveur.component';
 import { PageHeaderModule } from './../../shared';
 
 @NgModule({
-    imports: [CommonModule, ServeurRoutingModule, PageHeaderModule],
+    imports: [CommonModule, ServeurRoutingModule, PageHeaderModule, TranslateModule],
     declarations: [ServeurComponent]
 })
 export class ServeurModule {}
